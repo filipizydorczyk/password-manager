28b187a73a6b64772a237f84e29c0f22454fefd7
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2025-05-19 17:20:48 +0200

    fix: nedcyption/descryption works with hex strings

--- android/app/src/main/java/pl/filipizydorczyk/passwordmanager/AES256CBC.kt
+++ android/app/src/main/java/pl/filipizydorczyk/passwordmanager/AES256CBC.kt
@@ -1,20 +1,21 @@
 package pl.filipizydorczyk.passwordmanager
 
 import java.security.SecureRandom
-import java.util.Base64
 import javax.crypto.Cipher
 import javax.crypto.spec.IvParameterSpec
 import javax.crypto.spec.SecretKeySpec
 
-fun encryptString(plainText: String, key: ByteArray, iv: String): String? {
+
+fun encryptString(plainText: String, key: ByteArray, ivHexString: String): String? {
     try {
-        val ivBytes = iv.substring(0, 16).toByteArray()
+        val ivBytes = ivHexString.chunked(2).map { it.toInt(16).toByte() }.toByteArray()
+
         val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
         val secretKeySpec = SecretKeySpec(key, "AES")
         val ivParameterSpec = IvParameterSpec(ivBytes) // should be creating other iv in node?
         cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec)
         val encryptedBytes = cipher.doFinal(plainText.toByteArray())
-        return Base64.getEncoder().encodeToString(encryptedBytes)
+        return encryptedBytes.joinToString("") { "%02x".format(it) }
     } catch (e: Exception) {
         e.printStackTrace()
     }
@@ -29,14 +30,16 @@ fun encryptString(plainText: String, key: ByteArray): PasswordDataModel? {
     return PasswordDataModel(ivString, pass)
 }
 
-fun decryptString(encryptedText: String, key: ByteArray, iv: String): String? {
+fun decryptString(encryptedHexText: String, key: ByteArray, ivHexString: String): String? {
     try {
-        val ivBytes = iv.substring(0, 16).toByteArray()
+        val ivBytes = ivHexString.chunked(2).map { it.toInt(16).toByte() }.toByteArray()
+        val plainTextBytes = encryptedHexText.chunked(2).map { it.toInt(16).toByte() }.toByteArray()
+
         val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
         val secretKeySpec = SecretKeySpec(key, "AES")
         val ivParameterSpec = IvParameterSpec(ivBytes)
         cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, ivParameterSpec)
-        val decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedText))
+        val decryptedBytes = cipher.doFinal(plainTextBytes)
         return String(decryptedBytes)
     } catch (e: Exception) {
         e.printStackTrace()
